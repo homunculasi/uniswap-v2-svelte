e74f34114a182ae156018895954665ce779b3416
homunsulasi <homunculasi@gmail.com>
2026-02-23 11:15:20 +0900

    :tada: Initial commit with SvelteKit and Tailwind CSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-[--color-uniswap-dark] text-[--color-uniswap-text]">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex items-center justify-center min-h-screen">
+	<h1 class="text-4xl font-bold text-[--color-uniswap-pink]">Swap</h1>
+</div>
